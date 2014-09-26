0179ac6ea8bea507b58acd47639ed4c986279090
AdamHKaplan <AdamHKaplan@gmail.com>
2014-09-26 12:34:59 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+www.projecteuler.com
+
+I have noted which problems I was able to solve with mental math (not programmatically).  I might add programmatic solutions in the future.
---
+++ problem-17.rb
@@ -0,0 +1,39 @@
+# problem-17.rb
+
+def numbers_of_letters_in_first_nine_numbers # 1-9
+  [3,3,5,4,4,3,5,5,4]
+end
+
+def next_ten # 10-19
+  [3,6,6,8,8,7,7,9,8,8]
+end
+
+def numbers_of_letters_in_first_nineteen_numbers # 1-19
+  numbers_of_letters_in_first_nine_numbers + next_ten
+end
+
+def get_new_array_of_ten_numbers(to_add) # for all numbers with [2..9] in tens, and [0..9] in ones
+  [0,3,3,5,4,4,3,5,5,4].map{|x| x+to_add} 
+end
+
+def to_add_for_all_hundreds # adding "hundred and" for numbers in the hundreds - will subtract 3 later for the "hundred" #'s
+  numbers_of_letters_in_first_nine_numbers.map{|x| x+10}
+end
+
+
+def first_ninety_nine # array containing numbers of letters for # 1-99
+  to_return = numbers_of_letters_in_first_nineteen_numbers
+  [6,6,5,5,5,7,6,6].each do |x| # values in array correspond to # of letters added for each ten
+    to_return += get_new_array_of_ten_numbers(x)
+  end
+  to_return
+end
+
+def go # putting it all together
+  to_return = first_ninety_nine.reduce(:+) # sum of an array
+  to_add_for_all_hundreds.each do |x|
+    to_return += x * 100 - 3 + first_ninety_nine.reduce(:+) # the "-3" alluded to in line 19
+  end
+  puts to_return+11 # add 11 for one-thousand
+end
+go
