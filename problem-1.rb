2067f06f70b8166fdfc67e411ea381e0d99c2880
AdamHKaplan <AdamHKaplan@gmail.com>
2015-01-19 18:19:03 -0800

    actually do problem 1

--- problem-1.rb
+++ problem-1.rb
@@ -1,1 +1,13 @@
-# I did this problem in my head
+# problem-1.rb
+# find sum of all multiples of 3 or 5 btwn 1 and 1000
+
+def is_multiple(num)
+  if num == 1000
+    0
+  elsif num % 3 == 0 || num % 5 == 0
+    num + is_multiple(num+1)
+  else
+    is_multiple(num+1)
+  end
+end
+puts is_multiple(1)
